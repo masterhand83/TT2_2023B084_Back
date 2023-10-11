be763e6e1e76a81f1e295f0269d60f16a3b86d85
masterhand83 <robinmiguelsdj@gmail.com>
2023-10-10 18:06:57 -0600

    añadido manejador de lista de productos

--- handlers/getProductoListHandler.py
+++ handlers/getProductoListHandler.py
@@ -10,19 +10,8 @@ def get_producto_list_handler(req: func.HttpRequest) -> func.HttpResponse:
     statement = select(Producto)
     data = session.scalars(statement).all()
     for d in data:
-        print(d.marca)
-        d_dict = {
-            "codigo": d.codigo,
-            "nombre": d.nombre,
-            "precio_unitario": float(d.precio_unitario),
-            "existencias": d.existencias,
-            "descripcion": d.descripcion,
-            "marca": d.marca.marca,
-            "activo": d.activo
-        }
         lista.append(d.as_dict())
     session.close()
-    print(lista)
     return func.HttpResponse(
         json.dumps(lista),
         headers={"Content-Type": "application/json"},
